f3ccc1f03df4be6a4d1734302d9ffc3ef04f28c4
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-20 01:39:58 +0530

    Added Media Query in Header Section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,17 +9,38 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <div
-            className="navbar-brand mb-0 h1 logo"
+            className="navbar-brand mb-0 fs-bolder fw-3 h1 app-logo"
             onClick={() => history.push("/")}
           >
-            <img src={process.env.PUBLIC_URL + "/assets/logo.jpg"}></img>
-            Car's Safety
+            <div className="logo-img">
+              <img
+                src={process.env.PUBLIC_URL + "/assets/logo.jpg"}
+                alt="Logo"
+              ></img>
+            </div>
+            <span className="label">Car's Safety</span>
           </div>
-
-          <ul className="navLinks">
-            <li onClick={() => history.push("/quote")}>Get a quote</li>
-            <li onClick={() => history.push("/admin")}>Admin</li>
-          </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navLinks navbar-nav">
+              <li className="nav-item" onClick={() => history.push("/quote")}>
+                Get Quote
+              </li>
+              <li className="nav-item" onClick={() => history.push("/admin")}>
+                Admin
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </React.Fragment>
